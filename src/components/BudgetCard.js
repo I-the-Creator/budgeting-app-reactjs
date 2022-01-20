ffb6be656eed67f73589658648bf8be36503db1e
I-the-Creator <andrey.burluckiy@gmail.com>
2022-01-20 21:37:27 +0100

    Budget card componentadd

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,18 +1,24 @@
 import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
-export default function BudgetCard({ name, amount, max, gray }) {
-  
-  // setting Card component background depending on 'amount', set the gray - if amount > 0 
+export default function BudgetCard({
+  name,
+  amount,
+  max,
+  gray,
+  onAddExpenseClick,
+  hideButtons
+}) {
+  // setting Card component background depending on 'amount', set the gray - if amount > 0
   const setCardClassNames = () => {
     const classNames = [];
     if (amount > max) {
-      classNames.push("bg-danger", "bg-opacity-10")
+      classNames.push("bg-danger", "bg-opacity-10");
     } else if (gray) {
-      classNames.push("bg-light")
+      classNames.push("bg-light");
     }
     return classNames;
-  }
+  };
 
   return (
     <Card className={setCardClassNames().join(" ")}>
@@ -21,32 +27,44 @@ export default function BudgetCard({ name, amount, max, gray }) {
           <div className="me-2">{name}</div>
           <div className="d-flex align-items-baseline">
             {currencyFormatter.format(amount)}
-            <span className="text-muted fs-6 ms-1">
-              / {currencyFormatter.format(max)}
-            </span>
+            {/* if we have max - display that section, if not as in 'Uncategorized' - then not display */}
+            {max && (
+              <span className="text-muted fs-6 ms-1">
+                / {currencyFormatter.format(max)}
+              </span>
+            )}
           </div>
         </Card.Title>
-        <ProgressBar
-          className="rounded-pill"
-          variant={getProgressBarVariant(amount, max)}
-
-          min={0}
-          max={max}
-          now={amount}
-        />
-        <Stack direction="horizontal" gap="2" className="mt-4">
-          <Button variant="outline-primary" className="ms-auto">Add Expense</Button>
-          <Button variant="outline-secondary">View Expenses</Button>
-        </Stack>
+        {max && (
+          <ProgressBar
+            className="rounded-pill"
+            variant={getProgressBarVariant(amount, max)}
+            min={0}
+            max={max}
+            now={amount}
+          />
+        )} 
+        {!hideButtons && (
+          <Stack direction="horizontal" gap="2" className="mt-4">
+            <Button
+              variant="outline-primary"
+              className="ms-auto"
+              onClick={onAddExpenseClick}
+            >
+              Add Expense
+            </Button>
+            <Button variant="outline-secondary">View Expenses</Button>
+          </Stack>
+        )}
       </Card.Body>
     </Card>
   );
 }
 
-// to set up the ProgressBar color 
+// to set up the ProgressBar color
 function getProgressBarVariant(amount, max) {
   const ration = amount / max;
-  if(ration < 0.5) return "primary"
-  if(ration < 0.75) return "warning"
-  return "danger"
+  if (ration < 0.5) return "primary";
+  if (ration < 0.75) return "warning";
+  return "danger";
 }
